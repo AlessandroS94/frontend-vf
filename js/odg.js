9845d0ebbadd21a57b81fe118d0c71c8cb22ef98
Alessandro <alex_994@ymail.com>
2020-01-26 15:30:09 +0100

    View odg/ods

--- js/odg.js
+++ js/odg.js
@@ -7,29 +7,41 @@ function getODG() {
              url: "http://localhost:8080/Bacheca/api/board/ODG",
              success: function(dati){
 				var x = Cookies.get('auth');
-				
-				  for(var i = dati.length-1; i >=0; i--)
+				if (x == "yes")
 				  {
-					 x = '<tr><td>' + dati[i].id + '</td><td>' + dati[i].dataPubblicazione + '</td><td>' + dati[i].titolo + '</td>';
-					 x += '<td>' + dati[i].ufficio+ '</td><td><a class="item" href="http://localhost:8080/Bacheca/api/board/ODG/'+dati[i].id+'/stream">' + dati[i].nomeDocumento + '</a></td><td>' + dati[i].proprietario + '</td>';
-					 x += '<td><a onclick="deleteOdg()" href="#" class="btn btn-danger btn-circle"><i class="fas fa-trash"></i></a></td></tr>';
-					 $('#odgData').append($(x));
-				  } 
+					  for(var i = dati.length-1; i >=0; i--)
+				  		{
+					 	x = '<tr><td>' + dati[i].id + '</td><td>' + dati[i].dataPubblicazione + '</td><td>' + dati[i].titolo + '</td>';
+					 	x += '<td>' + dati[i].ufficio+ '</td><td><a class="item" href="http://localhost:8080/Bacheca/api/board/ODG/'+dati[i].id+'/stream">' + dati[i].nomeDocumento + '</a></td><td>' + dati[i].proprietario + '</td><td>' + dati[i].allegati + '</td>';
+					 	x += '<td><a onclick="deleteOdg('+dati[i].id+')" href="#" class="btn btn-danger btn-circle"><i class="fas fa-trash"></i></a></td></tr>';
+					 	$('#odgData').append($(x));
+				  		} 
+				  }
+				 else
+					 {
+						for(var i = dati.length-1; i >=0; i--)
+				  			{
+					 		x = '<tr><td>' + dati[i].id + '</td><td>' + dati[i].dataPubblicazione + '</td><td>' + dati[i].titolo + '</td>';
+					 		x += '<td>' + dati[i].ufficio+ '</td><td><a class="item" href="http://localhost:8080/Bacheca/api/board/ODG/'+dati[i].id+'/stream">' + dati[i].nomeDocumento + '</a></td><td>' + dati[i].proprietario + '<td></td></td>';
+					 		$('#odgData').append($(x));
+				  			} 
+					 }
 				 }
-				
-         });
+			});
 }
 
 getODG();
 
 
-/*function deleteOdg(id){
+
+//Delete a row ODG
+function deleteOdg(id){
 	$.ajax({ 
              type: "DELETE",
              contentType: "application/json",
              url: "http://localhost:8080/Bacheca/api/board/ODG/"+id,
              success: function(dati){
-				
+				window.location('odg.html');
              }
          });
-}*/
+}
